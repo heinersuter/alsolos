a43521b82d2a95b4c5778498631f89608ec69bbc
hsu <hsu@zuehlke.com>
2013-10-01 14:15:29 +0200

    First steps for Buzzer with Firebase.

---
+++ Alsolos.Buzzer/Alsolos.Buzzer/js/ApplicationController.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module("buzzer", ["firebase"]);
+
+function ApplicationController($scope, angularFire) {
+    var ref = new Firebase("https://alsolos.firebaseIO.com/buzzer");
+    $scope.messages = [];
+    angularFire(ref, $scope, "messages");
+
+    $scope.addMessage = function (e) {
+        if (e.keyCode != 13) return;
+        $scope.messages.push({ from: $scope.name, body: $scope.msg });
+        $scope.msg = "";
+    };
+}
