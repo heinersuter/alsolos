b4e560bbc0a35d0968b0dda5d3c1faad151877b4
Heiner Suter <heiner.suter@gmx.ch>
2013-10-01 15:52:48 +0200

    Buzzer!

--- Alsolos.Buzzer/Alsolos.Buzzer/js/ApplicationController.js
+++ Alsolos.Buzzer/Alsolos.Buzzer/js/ApplicationController.js
@@ -1,13 +1,37 @@
 ﻿var app = angular.module("buzzer", ["firebase"]);
 
 function ApplicationController($scope, angularFire) {
-    var ref = new Firebase("https://alsolos.firebaseIO.com/buzzer");
-    $scope.messages = [];
-    angularFire(ref, $scope, "messages");
+    var fbUsersRef = new Firebase("https://alsolos.firebaseIO.com/buzzer/game/users");
+    $scope.users = [];
+    angularFire(fbUsersRef, $scope, "users");
 
-    $scope.addMessage = function (e) {
-        if (e.keyCode != 13) return;
-        $scope.messages.push({ from: $scope.name, body: $scope.msg });
+    var fbWinnerRef = new Firebase("https://alsolos.firebaseIO.com/buzzer/game/winner");
+    $scope.winner = "";
+    angularFire(fbWinnerRef, $scope, "winner");
+
+    $scope.isJoinDisabled = false;
+    $scope.isBuzzerDisabled = true;
+    $scope.isResetDisabled = true;
+
+    $scope.addUser = function () {
+        if ($scope.users.indexOf($scope.userName) != -1) {
+            return;
+        }
+        $scope.isJoinDisabled = true;
+        $scope.isBuzzerDisabled = false;
+        $scope.users.push($scope.userName);
         $scope.msg = "";
+    };
+
+    $scope.buzzer = function () {
+        $scope.isBuzzerDisabled = true;
+        if ($scope.winner == "") {
+            $scope.winner = $scope.userName;
+        }
+    };
+
+    $scope.reset = function () {
+        $scope.isBuzzerDisabled = false;
+        $scope.winner = "";
     };
 }
