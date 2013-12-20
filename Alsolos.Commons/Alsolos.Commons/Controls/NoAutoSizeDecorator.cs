fe6bc68de392a3dd77fbeb7e09902ad28a18a08f
Heiner Suter <heiner.suter@gmx.ch>
2013-12-20 09:15:53 +0100

    New Shared Resource Dictionary.

--- Alsolos.Commons/Alsolos.Commons/Controls/NoAutoSizeDecorator.cs
+++ Alsolos.Commons/Alsolos.Commons/Controls/NoAutoSizeDecorator.cs
@@ -15,32 +15,32 @@
             "KeepWidth", typeof(bool), typeof(NoAutoSizeDecorator), new PropertyMetadata(false));
 
         public bool KeepWidth {
-            get { return (bool)this.GetValue(KeepWidthProperty); }
-            set { this.SetValue(KeepWidthProperty, value); }
+            get { return (bool)GetValue(KeepWidthProperty); }
+            set { SetValue(KeepWidthProperty, value); }
         }
 
         public static readonly DependencyProperty KeepHeightProperty = DependencyProperty.Register(
             "KeepHeight", typeof(bool), typeof(NoAutoSizeDecorator), new PropertyMetadata(false));
 
         public bool KeepHeight {
-            get { return (bool)this.GetValue(KeepHeightProperty); }
-            set { this.SetValue(KeepHeightProperty, value); }
+            get { return (bool)GetValue(KeepHeightProperty); }
+            set { SetValue(KeepHeightProperty, value); }
         }
 
         protected override Size MeasureOverride(Size availableSize) {
-            var innerWidth = this.KeepWidth ? availableSize.Width : Math.Min(this._lastFinalSize.Width, availableSize.Width);
-            var innerHeight = this.KeepHeight ? availableSize.Height : Math.Min(this._lastFinalSize.Height, availableSize.Height);
+            var innerWidth = KeepWidth ? availableSize.Width : Math.Min(_lastFinalSize.Width, availableSize.Width);
+            var innerHeight = KeepHeight ? availableSize.Height : Math.Min(_lastFinalSize.Height, availableSize.Height);
             Child.Measure(new Size(innerWidth, innerHeight));
 
-            var outerWidth = this.KeepWidth ? Child.DesiredSize.Width : 0;
-            var outerHeight = this.KeepHeight ? Child.DesiredSize.Height : 0;
+            var outerWidth = KeepWidth ? Child.DesiredSize.Width : 0;
+            var outerHeight = KeepHeight ? Child.DesiredSize.Height : 0;
             return new Size(outerWidth, outerHeight);
         }
 
         protected override Size ArrangeOverride(Size finalSize) {
             if (_lastFinalSize != finalSize) {
                 _lastFinalSize = finalSize;
-                this.MeasureOverride(_infinitySize);
+                MeasureOverride(_infinitySize);
             }
             Child.Arrange(new Rect(finalSize));
             return finalSize;
