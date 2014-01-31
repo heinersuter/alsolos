29e86ad7ccb60a88a3e852fb9a508bd852dc1a62
Heiner Suter <heiner.suter@gmx.ch>
2014-01-31 14:47:34 +0100

    SimpleStretchPanel.

---
+++ Alsolos.Commons/Alsolos.Commons/Controls/SimpleStretchPanel/SimpleStretchPanelMode.cs
@@ -0,0 +1,10 @@
+﻿namespace Alsolos.Commons.Controls.SimpleStretchPanel {
+    public enum SimpleStretchPanelMode {
+        VerticalStretchTop,
+        VerticalStretchCenter,
+        VerticalStretchBottom,
+        HorizontalStretchLeft,
+        HorizontalStretchCenter,
+        HorizontalStretchRight,
+    }
+}
