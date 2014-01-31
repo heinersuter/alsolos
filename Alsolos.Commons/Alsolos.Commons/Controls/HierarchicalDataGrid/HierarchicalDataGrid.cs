d32ba6794d637731718fc1410dbf326056d21f91
Heiner Suter <heiner.suter@gmx.ch>
2014-01-31 15:14:37 +0100

    SimpleStretchPanel.

--- Alsolos.Commons/Alsolos.Commons/Controls/HierarchicalDataGrid/HierarchicalDataGrid.cs
+++ Alsolos.Commons/Alsolos.Commons/Controls/HierarchicalDataGrid/HierarchicalDataGrid.cs
@@ -1,7 +1,9 @@
 ﻿namespace Alsolos.Commons.Controls.HierarchicalDataGrid {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.ComponentModel;
+    using System.Linq;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Data;
@@ -9,81 +11,87 @@
     public class HierarchicalDataGrid : DataGrid {
         public static readonly DependencyProperty HierarchicalDataGridItemsProperty = DependencyProperty.Register(
             "HierarchicalDataGridItems",
-            typeof(IList<IHierarchicalDataGridItem>),
+            typeof(IEnumerable),
             typeof(HierarchicalDataGrid),
             new PropertyMetadata(new List<IHierarchicalDataGridItem>(), OnHierarchicalDataGridItemsChanged));
 
-        public IList<IHierarchicalDataGridItem> HierarchicalDataGridItems {
-            get { return (IList<IHierarchicalDataGridItem>)this.GetValue(HierarchicalDataGridItemsProperty); }
-            set { this.SetValue(HierarchicalDataGridItemsProperty, value); }
+        public IEnumerable HierarchicalDataGridItems {
+            get { return (IEnumerable)GetValue(HierarchicalDataGridItemsProperty); }
+            set { SetValue(HierarchicalDataGridItemsProperty, value); }
         }
 
         private static void OnHierarchicalDataGridItemsChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e) {
             var grid = (HierarchicalDataGrid)sender;
-            grid.AddItems((IList<IHierarchicalDataGridItem>)e.NewValue);
+            var items = (IEnumerable)e.NewValue;
+            if (items != null) {
+                var dataGridItems = items.Cast<IHierarchicalDataGridItem>();
+                grid.AddItems(dataGridItems);
+            } else {
+                grid.AddItems(null);
+            }
         }
 
         protected override void OnInitialized(EventArgs e) {
             // Disable sorting for all columns
-            foreach (var column in this.Columns) {
+            foreach (var column in Columns) {
                 column.CanUserSort = false;
             }
 
             // Add column with indented expander buttons
             var resourceDictionary = (ResourceDictionary)Application.LoadComponent(new Uri("/ExpandableGridControl;component/Control/HierarchicalDataGrid.xaml", UriKind.RelativeOrAbsolute));
-            this.Columns.Insert(0, new DataGridTemplateColumn { CellTemplate = (DataTemplate)resourceDictionary["ExpanderCellTemplate"] });
+            Columns.Insert(0, new DataGridTemplateColumn { CellTemplate = (DataTemplate)resourceDictionary["ExpanderCellTemplate"] });
 
             base.OnInitialized(e);
         }
 
         public void ExpandAll() {
-            foreach (var item in this.Items) {
+            foreach (var item in Items) {
                 var wrapper = (HierachicalDataGridItemWrapper)item;
                 wrapper.ExpandRecursively();
             }
         }
 
         public void CollapseAll() {
-            foreach (var item in this.Items) {
+            foreach (var item in Items) {
                 var wrapper = (HierachicalDataGridItemWrapper)item;
                 wrapper.CollapseRecursively();
             }
         }
 
         private void AddItems(IEnumerable<IHierarchicalDataGridItem> items) {
-            foreach (var item in this.Items) {
+            foreach (var item in Items) {
                 var wrapper = (HierachicalDataGridItemWrapper)item;
-                wrapper.PropertyChanged -= this.OnWrapperPropertyChanged;
+                wrapper.PropertyChanged -= OnWrapperPropertyChanged;
             }
-            this.Items.Clear();
+            Items.Clear();
 
             if (items == null) {
                 return;
             }
             foreach (var item in items) {
                 var wrapper = HierachicalDataGridItemWrapper.CreateRecursively(item);
-                this.AddItemRecursively(wrapper);
+                AddItemRecursively(wrapper);
                 wrapper.IsParentExpanded = true;
                 wrapper.IsExpanded = false;
             }
         }
 
         private void AddItemRecursively(HierachicalDataGridItemWrapper wrapper) {
-            this.Items.Add(wrapper);
-            wrapper.PropertyChanged += this.OnWrapperPropertyChanged;
+            Items.Add(wrapper);
+            wrapper.PropertyChanged += OnWrapperPropertyChanged;
             foreach (var child in wrapper.Children) {
-                this.AddItemRecursively(child);
+                AddItemRecursively(child);
             }
         }
 
         private void OnWrapperPropertyChanged(object sender, PropertyChangedEventArgs e) {
             if (e.PropertyName == "IsParentExpanded") {
-                this.ApplyFilter();
+                ApplyFilter();
             }
         }
 
         private void ApplyFilter() {
-            var collectionView = CollectionViewSource.GetDefaultView(this.Items);
+            var collectionView = CollectionViewSource.GetDefaultView(Items);
             collectionView.Filter = Filter;
             collectionView.Refresh();
         }
