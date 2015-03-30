6a2f1cdfcc5fffe0e45abf67600335d0b41efcd0
Heiner <heiner.suter@gmx.ch>
2015-03-30 14:00:43 +0200

    Path for current machine.

--- Alsolos.AttendanceRecorder/Alsolos.AttendanceRecorder.WindowsService/install.bat
+++ Alsolos.AttendanceRecorder/Alsolos.AttendanceRecorder.WindowsService/install.bat
@@ -1,6 +1,6 @@
 echo.
 
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /i C:\Users\taasuhe2\progis\alsolos\Alsolos.AttendanceRecorder\Alsolos.AttendanceRecorder.WindowsService\bin\Debug\Alsolos.AttendanceRecorder.WindowsService.exe
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /i C:\Users\hsu\Documents\progis\alsolos\Alsolos.AttendanceRecorder\Alsolos.AttendanceRecorder.WindowsService\bin\Debug\Alsolos.AttendanceRecorder.WindowsService.exe
 
 echo.
 pause
